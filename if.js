7bd4b48a2e1c51c68893a6cef91747e6d6ffe148
Susmita <sinhasusmita725@gmail.com>
2023-09-02 16:46:40 +0530

    day4

---
+++ if.js
@@ -0,0 +1,11 @@
+var x=100;
+var y="100";
+/*if (y>x) {
+    document.write("Y is greater");    
+}*/
+/*if (x===y) {
+    document.write("It is true");
+}*/
+/*if (x==y) {
+    document.write("Susmita Sinha");
+}*/
