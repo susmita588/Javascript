f386f5d1e60fb75313bf638906a4f54124d1a205
Susmita <sinhasusmita725@gmail.com>
2023-09-17 21:16:57 +0530

    day9

---
+++ dom.js
@@ -0,0 +1,28 @@
+// for targetting the DOM( DOCUMENT OBJECT METHOD),it is another important part of HTML , it is a strong point of javascript
+// how i targets it 
+//special method of documnets
+//id----  document.getElementById(id)
+//class name ----document.getElementsByClassName(name)
+// tagname -------document.getElementByTagName(name)
+
+var element;
+
+//element = document;// it shows all things under documnet that means html
+//element = document.all;// it shows all  tags in array format
+//element = document.all[9];// individual dekha r jonno
+//element = document.head; // shows head tag
+//element = document.title;// title er modhye ki lekha ache seta dekhalo
+//element = document.body;//body dekha r jonno
+//element = document.links;//linkgulo dekhanor jonno
+// aki bhabe image o forms bar korte pari
+//element = document.doctype;// doctype dekhanor jonno
+//element = document.url; // url khoje
+//element = document.domain; // ip address return kore
+//element = document.baseURI; // puro url ta ip address somet return kore
+// main things --------
+//element = document.getElementById("navbar");
+//element = document.getElementsByClassName("container");
+//element = document.getElementsByClassName("side-panel")[0];
+//element = document.getElementsByTagName("a");
+ //element = document.getElementsByTagName("a")[9];
+console.log(element);
